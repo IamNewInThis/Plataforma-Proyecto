afbb7bf62329d6a0e3d8c984f41c6f3c8ca495a3
Nicolas <ni.ruizz@duocuc.cl>
2023-04-26 20:02:01 -0400

    Front End

---
+++ src/scenes/productos/index.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from 'react'
+import {
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    Collapse,
+    Button,
+    Typography,
+    Rating,
+    useTheme,
+    useMediaQuery,
+}from "@mui/material"
+
+import Header from 'components/Header'
+
+const Productos = () => {
+    return (
+        <Box>
+            <Header titulo={"Productos"}></Header>
+        </Box>
+    )
+}
+
+export default Productos
