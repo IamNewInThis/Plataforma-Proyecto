434dead9e172219e0ae1cfd04563f802ac0c3136
Nicolas <ni.ruizz@duocuc.cl>
2023-05-27 21:47:52 -0400

    API SWAGGER Y README CON INSTALACION

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,23 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerSpec = {
+    definition:{
+        openapi: '3.0.0',
+        info: {
+            title: "Node MongoDB API",
+            version: "1.0.0"
+        },
+        servers:[
+            {
+                url: 'http://localhost:3001/'
+            }
+        ]
+    },
+    apis:[`${path.join(__dirname,"./routes/task.routes.js")}`]
+}
 
 const { Mongoose } = require('./database')
 //setting
@@ -17,6 +34,11 @@ app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(bodyParser.text({ limit: '200mb' }));
 app.use(cors());
+
+//SWAGGER API
+app.use('/api-doc',swaggerUI.serve,swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
+
+
 //Routes
 app.use('/api' ,require('./routes/task.routes'));
 //static files 
--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -3,10 +3,85 @@ const router = express.Router();
 
 const Task = require('../models/task');
 
-router.get('/', async (req, res) => {
+//GET
+
+// create user
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Productos:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: id del producto
+ *         nombre:
+ *           type: string
+ *           description: nombre del producto
+ *         marca:
+ *           type: string
+ *           description: marca del producto
+ *         precio:
+ *           type: number
+ *           description: precio del producto
+ *         categoria:
+ *           type: string
+ *           description: categoria del producto
+ *         stock:
+ *           type: number
+ *           description: stock del producto
+ *         sub_categoria:
+ *           type: string
+ *           description: Subcategoria del producto
+ *         img:
+ *           type: string
+ *           description: imagen del producto
+ *       required:
+ *         - id
+ *         - nombre
+ *         - marca
+ *         - precio
+ *         - categoria
+ *         - stock
+ *         - sub_categoria
+ *         - img
+ *       example:
+ *         id: 1
+ *         nombre: penedol
+ *         marca: pa tu estres
+ *         precio: 56
+ *         categoria: jaja
+ *         stock: 56
+ *         sub_categoria: vamo hacer
+ *         img: par de weas
+ */
+
+router.get('/productos', async (req, res) => {
     const tasks = await Task.find();
     res.json(tasks)
 });
+
+//GET ALL
+
+// 
+/**
+ * @swagger
+ * /api/productos:
+ *   get:
+ *     summary: return all productos
+ *     tags: [Productos]
+ *     responses:
+ *       200:
+ *         description: all productos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Productos'
+ */ 
+
 
 router.get('/:id', async (req, res) => {
     const tasks = await Task.findById(req.params.id);
